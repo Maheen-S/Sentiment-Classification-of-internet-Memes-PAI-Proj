8a659943c06e280cc2a39c1d9e37937af95f91b1
Maheen-S <166301365+Maheen-S@users.noreply.github.com>
2024-04-07 17:41:27 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,83 @@
+from flask import Flask, render_template, request
+
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+@app.route('/')
+def create_tables():
+    return db.create_all()
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue adding your task'
+
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+
+    if request.method == 'POST':
+        task.content = request.form['content']
+
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue updating your task'
+
+    else:
+        return render_template('update.html', task=task)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+    
+# from keras.preprocessing.image import load_img
+# from keras.preprocessing.image import img_to_array
+# from keras.applications.vgg16 import preprocess_input
+# from keras.applications.vgg16 import decode_predictions
+# from keras.applications.vgg16 import VGG16
+
+# app=Flask(__name__)
+
+# @app.route('/', methods=['GET'])
+
+# @app.route('/', methods=['POST'])
+# def predict():
+#     image_file=request.files['image_file']
+#     image_path='./images' +image_file.filename
+#     image_file.save(image_path)
+
+#     image= load_img(image_path + target_size= "any")
+#     image=img_to_array(image)
+#     return render_template('index.html,', prediction ='classification')
+
+# if __name__== '__main__':
+#     app.run(port=3000, debug=True)
